89b1e7d8fb123e3310cb36c2c5aba24ecf7d0c2a
Jose Ariel Romero <josea132.romero@gmail.com>
2020-05-20 14:18:23 -0400

    Removing Mapper class. Renamed xml_to_ordereddict function to xml_to_json. Added docstring to xml_to_json function in utils.mapper.

--- jromero132_ontology_learning_bachelor_thesis/utils/mapper.py
+++ jromero132_ontology_learning_bachelor_thesis/utils/mapper.py
@@ -1,7 +1,17 @@
+from typing import OrderedDict
+
 import xmltodict
 
 
-class Mapper:
-    @staticmethod
-    def xml_to_ordereddict(xml_text):
-        return xmltodict.parse(xml_text)
+def xml_to_json(xml_text: str) -> OrderedDict:
+    """Converts xml text to json.
+
+    Args:
+        xml_text (str): xml text to be parsed
+
+    Returns:
+        OrderedDict: an ordered dict representing the xml text as json
+
+    Raises:
+    """
+    return xmltodict.parse(xml_text)
