53adc945bd8c75dbe1eaf6e4661e33ff33810a59
Jose Ariel Romero <josea132.romero@gmail.com>
2020-05-21 19:23:04 -0400

    Added docstrings and annotations to  MedlineArticleMetadata class.

--- ontology_learning/data_type/medline/medline_article/medline_article_metadata.py
+++ ontology_learning/data_type/medline/medline_article/medline_article_metadata.py
@@ -1,14 +1,28 @@
 class MedlineArticleMetadata:
+	"""Metadata of medline articles used in MedlineArticle class.
+
+	Args:
+		url (str): url of the medline article
+		language (str): language of the medline article
+		title (str): title of the medline article
+		description (str): description of the medline article
+		id (str): id of the medline article
+		date_created (str): creation date of the medline article
+		also_called (list): other names of the medline article
+
+	Returns:
+		None
+	"""
 	def __init__(
 		self,
 		*,
-		url = None,
-		language = None,
-		title = None,
-		description = None,
-		id = None,
-		date_created = None,
-		also_called = None,
+		url: str = None,
+		language: str = None,
+		title: str = None,
+		description: str = None,
+		id: str = None,
+		date_created: str = None,
+		also_called: list = None,
 	):
 		self.url = url
 		self.language = language
