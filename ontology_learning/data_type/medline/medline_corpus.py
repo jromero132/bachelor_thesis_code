39b7b2a61e9abe8316b9d6e5f1066f72084530d8
Jose Ariel Romero <josea132.romero@gmail.com>
2020-05-31 15:10:56 -0400

    Added code to MedlineCorpus class

--- ontology_learning/data_type/medline/medline_corpus.py
+++ ontology_learning/data_type/medline/medline_corpus.py
@@ -1,20 +1,32 @@
+# built-in packages
+from typing import Generator
+
+# own modules
+from ontology_learning.data_type.medline.medline_article import MedlineArticle
+
 class MedlineCorpus:
-	def __init__(self, date_generated, *, articles = []):
-		self._date_generated = date_generated
+	"""Represents a corpus using medline articles as documents
+
+	Args:
+		date_generated (str): generation date of the corpus
+		articles (list): initial list of articles (documents)
+
+	Returns:
+		None
+	"""
+	def __init__(self, date_generated: str, *, articles: list = []) -> None:
+		self.date_generated = date_generated
 		self._articles = articles
 
 	# magic methods
-	def __len__(self):
+	def __len__(self) -> int:
+		"""
+		"""
 		return len(self._articles)
 
-	def __iter__(self):
+	def __iter__(self) -> Generator:
 		return (article for article in self._articles)
 
-	# instance properties
-	@property
-	def date_generated(self):
-		return self._date_generated
-
 	# instance methods
-	def add_health_topic(medline_article):
+	def add_health_topic(medline_article: MedlineArticle) -> None:
 		self._articles.append(medline_article)
