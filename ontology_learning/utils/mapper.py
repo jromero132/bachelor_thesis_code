a2bf7a131fc5726c5d911339687c0210bef806da
Jose Ariel Romero <josea132.romero@gmail.com>
2020-06-12 16:54:06 -0400

    Added docstrings and annotations to utils.mapper file.

--- ontology_learning/utils/mapper.py
+++ ontology_learning/utils/mapper.py
@@ -1,12 +1,12 @@
-# built-in libraries
+# built-in packages
 from typing import OrderedDict
 
 import re
 
-# third party libraries
+# third party packages
 import xmltodict
 
-# module libraries
+# own modules
 from ontology_learning.data_type.medline.medline_article import MedlineArticle
 from ontology_learning.data_type.medline.medline_article.medline_article_metadata import MedlineArticleMetadata
 from ontology_learning.data_type.medline.medline_corpus import MedlineCorpus
@@ -23,13 +23,32 @@ def xml_to_json(xml_text: str) -> OrderedDict:
     """
     return xmltodict.parse(xml_text)
 
-def _html_text_sanitizer(html):
-    text = re.sub(r"<.+?>", "", html)
-    lines = re.split(r"\r+", text)
-    sanitized_lines = (re.sub(r"\s{2,}", " ", line).strip() for line in lines)
+def _html_text_sanitizer(html: str) -> str:
+    """Removes all tags in an html text
+
+    Args:
+        html (str): html text to remove tags
+
+    Returns:
+        str: html text without tags
+    """
+    text: str = re.sub(r"<.+?>", "", html)
+    lines: list = re.split(r"\r+", text)
+    sanitized_lines: list = (
+        re.sub(r"\s{2,}", " ", line).strip()
+        for line in lines
+    )
     return "\n".join(sanitized_lines)
 
 def json_to_medlinearticle(json_dict: dict) -> MedlineArticle:
+    """Converts a medline article from json to MedlineArticle
+
+    Args:
+        json_dict (OrderedDict): json representing a medline article
+
+    Returns:
+        MedlineArticle: medline article object
+    """
     return MedlineArticle(
         text = _html_text_sanitizer(json_dict.get("full-summary", None)),
         meta = MedlineArticleMetadata(
@@ -44,14 +63,15 @@ def json_to_medlinearticle(json_dict: dict) -> MedlineArticle:
     )
 
 def json_to_medlinecorpus(json_dict: dict) -> MedlineCorpus:
-    """
+    """Converts all medline articles in json to a MedlineCorpus
 
     Args:
+        json_dict (OrderedDict): json with medline articles
 
     Returns:
+        MedlineCorpus: medline corpus with all articles in json
     """
-    health_topics = json_dict[ "health-topics" ]
-    print(health_topics["health-topic"][0].keys())
+    health_topics: OrderedDict = json_dict[ "health-topics" ]
     return  MedlineCorpus(
                 date_generated = health_topics[ "@date-generated" ],
                 articles = [
