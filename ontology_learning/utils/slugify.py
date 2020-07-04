c835a2c1a5804f4eae789bcf3f79df6eef454d44
Jose Ariel Romero <josea132.romero@gmail.com>
2020-07-03 23:44:22 -0400

    Added normalizer string function to utils/slugify.py

--- ontology_learning/utils/slugify.py
+++ ontology_learning/utils/slugify.py
@@ -1,12 +1,13 @@
 import re
+import unidecode
 
 
 def decode(text: str) -> str:
-	return text
+	return unidecode.unidecode(text)
 
 def slugify(text: str) -> str:
-	lowercased_text: str = text.lower()
-	decoded_text: str = decode(lowercased_text)
-	slugified_text: str = re.sub(r"[\W_]+", "-", decoded_text)
-	stripped_text = slugified_text.strip()
+	decoded_text: str = decode(text)
+	lowercased_text: str = decoded_text.lower()
+	slugified_text: str = re.sub(r"[\W_]+", "-", lowercased_text)
+	stripped_text = slugified_text.strip("-")
 	return stripped_text
