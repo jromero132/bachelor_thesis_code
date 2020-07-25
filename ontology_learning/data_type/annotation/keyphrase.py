8e45a19e4a58c8066da9e45a7b6f999dbc966461
Jose Ariel Romero <josea132.romero@gmail.com>
2020-07-25 14:38:16 -0400

    Added clone method and attributes in constructor of Keyphrase class.

--- ontology_learning/data_type/annotation/keyphrase.py
+++ ontology_learning/data_type/annotation/keyphrase.py
@@ -1,10 +1,10 @@
 class Keyphrase:
-	def __init__(self, id: str, label: str, spans: "List[Tuple(int, int)]", text: str):
+	def __init__(self, id: str, label: str, spans: "List[Tuple(int, int)]", text: str, *, attributes = None):
 		self.id = id
 		self.label = label
 		self.spans = spans
 		self.text = text
-		self.attributes: "List[Attribute]" = []
+		self.attributes: "List[Attribute]" = attributes or []
 
 	def __repr__(self):
 		return f"Keyphrase(text={self.text}, label={self.label}, id={self.id}, attr={self.attributes})"
@@ -14,3 +14,6 @@ class Keyphrase:
 
 	def as_ann(self, shift: int) -> str:
 		return f"{self.id}\t{self.label} {';'.join(f'{start + shift} {end + shift}' for start, end in self.spans)}\t{self.text}"
+
+	def clone(self):
+		return Keyphrase(self.id, self.label, self.spans.copy(), self.text, attributes = self.attributes.copy())
