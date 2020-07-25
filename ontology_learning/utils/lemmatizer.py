3d52010794cf1a2d00a083351a4dd852efa56a5b
Jose Ariel Romero <josea132.romero@gmail.com>
2020-07-25 14:43:12 -0400

    Added lemmatizer to utils.

---
+++ ontology_learning/utils/lemmatizer.py
@@ -0,0 +1,11 @@
+import spacy
+
+
+class Lemmatizer:
+	def __init__(self, *, model = "es_core_news_md"):
+		self.model = model
+		self.nlp = spacy.load(self.model)
+
+	def lemmatize(self, text):
+		return " ".join(word.lemma_ for word in self.nlp(text))
+		# return text
